83c79eb8b397b1622d4d24c396ae9d48232a2a59
seanedw1 <sedwards@fullsail.edu>
2016-05-09 18:50:30 -0400

    still fixing firebase

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var smsapp1 = angular.module('starter', ['ionic', 'starter.controllers','firebase']);
+var smsapp1 = angular.module('starter', ['ionic', 'starter.controllers','firebase'])
+.constant('FIREBASE_URL', 'https://smsapp1.firebaseio.com/');
 
 smsapp1.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -36,7 +37,8 @@ smsapp1.run(function($ionicPlatform) {
     url: '/register',
     views: {
       'menuContent': {
-        templateUrl: 'templates/register.html'
+        templateUrl: 'templates/register.html',
+        controller:'RegistrationController'
       }
     }
   })
@@ -66,26 +68,7 @@ smsapp1.run(function($ionicPlatform) {
           templateUrl: 'templates/browse.html'
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/search');
 });
